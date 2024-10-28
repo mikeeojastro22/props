49f7553de09240c3601fae0bc3b1801a109e4a8e
Mikee Ojastro <mikeeojastro@gmail.com>
2024-10-28 20:02:50 +0800

    October 28, 2024

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
 import User from './components/User/User';
+import bankUsers from './assets/bankUsers.json';
 
 function App() {
 
   const bankEmployee = {
-
+    name: "Patrick Star",
+    balance: 1000000,
+    email: "patrick@email.com"
   }
 
   const address = {
-
+    addressNo: 3509,
+    street: "Star Street",
+    city: "Bikini Bottom"
   }
 
   return (
     <div className="App">
       <SearchBar></SearchBar>
-      <User></User>
-
+      {/* Whatever attribute we place in the component, it will be referred to as props */}
+      {/* syntax is variableNameToBeAccessedInTheComponent={variable to be passed} */}
+      {/* <User employee={bankEmployee} address={address} /> */}
+      <User employee={bankEmployee} address={address}></User>
+      {
+        // bankUsers - array
+        // map - traverses through our array
+        // user - representation of each element in our bankUsers array
+        bankUsers.map((user) => {
+          return <div key={user.id}> {user.name} {user.balance} </div>
+        })
+      }
     </div>
   );
 }
--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,10 +1,17 @@
 import './User.css';
 
+// props work like a parameter
 function User(props){
+    const { employee, address } = props;
+    const { name, balance, email } = employee;
+    const { addressNo, street, city } = address;
 
     return (
         <div className='user'>
-
+            <h1>{name} <i className="fa-solid fa-pencil"></i></h1>
+            <h1>{balance}</h1>
+            <h1>{email}</h1>
+            <h1>{addressNo} {street} {city}</h1>
         </div>
     );
 }
